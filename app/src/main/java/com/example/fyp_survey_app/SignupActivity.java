c3ea08043f6feba1d2feb00eaccc2dbf83c8769a
Setsuna Cheng <setsuna@Setsunas-MacBook-Pro.local>
2019-02-20 17:46:58 +0800

    Add doctor and patient sign up Ui and functions

--- app/src/main/java/com/example/fyp_survey_app/SignupActivity.java
+++ app/src/main/java/com/example/fyp_survey_app/SignupActivity.java
@@ -2,6 +2,7 @@ package com.example.fyp_survey_app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class SignupActivity extends Activity {
@@ -11,5 +12,32 @@ public class SignupActivity extends Activity {
         setContentView(R.layout.signup_activity);
 
         Button doctorButton = (Button)findViewById(R.layout.signup_activity.doctor);
+        Button patientButton = (Button)findViewById(R.layout.signup_activity.patient);
+
+        doctorButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                doctorSignUp();
+            }
+        });
+        patientButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                patientSignUp();
+            }
+        });
+    }
+
+
+    //TODO: doctor sign up function
+    public void doctorSignUp(){
+        setContentView(R.layout.doctor_signup_layout);
+    }
+
+    //TODO: patient sign up function
+    public void patientSignUp(){
+        setContentView(R.layout.patient_signup_layout);
     }
 }
